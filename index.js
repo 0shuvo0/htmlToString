5eb498f18e825925146f34bcbb2efa3f412779ce
0shuvo0 <seriouslolz1@gmail.com>
2022-02-07 09:41:21 +0600

    resolved extra space issue

--- index.js
+++ index.js
@@ -8,16 +8,18 @@ function htmlToString(el, tab = "\t"){
     (typeof el === 'string') && (el = document.querySelector(el))
     
     let tagName = el.tagName.toLowerCase()
+    let attributes = getAttributes(el)
+    attributes = attributes.length ? ` ${attributes.join(' ')}` : ''
 
     if(selfClosingTags.includes(tagName)){
-        return `<${tagName} ${getAttributes(el).join(" ")} />`
+        return `<${tagName}${attributes} />`
     }
 
     let childs = [...el.querySelectorAll('*')]
     if(childs.length === 0){
-        return `<${tagName} ${getAttributes(el).join(" ")}>${el.innerText}</${tagName}>`
+        return `<${tagName}${attributes}>${el.innerText}</${tagName}>`
     }
-    return `<${tagName} ${getAttributes(el).join(" ")}>\n${childs.map(cEl => tab + htmlToString(cEl, tab + "\t")).join("\n")}\n${tab.slice(1)}</${tagName}>`
+    return `<${tagName}${attributes}>\n${childs.map(cEl => tab + htmlToString(cEl, tab + "\t")).join("\n")}\n${tab.slice(1)}</${tagName}>`
 }
 
 export default htmlToString
